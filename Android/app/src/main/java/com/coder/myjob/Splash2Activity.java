a7333b5044fb4e508c00c5eb5b51d5759d34c213
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2022-08-03 22:07:01 +0700

    Splash Activity 2

---
+++ Android/app/src/main/java/com/coder/myjob/Splash2Activity.java
@@ -0,0 +1,14 @@
+package com.coder.myjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+    }
+}
